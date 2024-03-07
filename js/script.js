e1ad57d90652e9db584c830c91c7953dc2863705
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-03-07 17:09:26 +0100

    [add] JSnack 1 total done

--- js/script.js
+++ js/script.js
@@ -1,55 +1,27 @@
 console.log('js-jsnacks-blocco-5');
 
 // JSnack 1
-
 const zucchine = [
-  {
-    peso: '120',
-    lunghezza: '8',
-  },
-
-  {
-    peso: '100',
-    lunghezza: '7',
-  },
-
-  {
-    peso: '140',
-    lunghezza: '9',
-  },
-
-  {
-    peso: '160',
-    lunghezza: '10',
-  },
-
-  {
-    peso: '200',
-    lunghezza: '12',
-  },
-
-  {
-    peso: '250',
-    lunghezza: '14',
-  },
-
-  {
-    peso: '270',
-    lunghezza: '13',
-  },
-
-  {
-    peso: '300',
-    lunghezza: '15',
-  },
-
-  {
-    peso: '350',
-    lunghezza: '16',
-  },
-
-  {
-    peso: '70',
-    lunghezza: '5',
-  }
+  {peso: '120',lunghezza: '8',},
+  {peso: '100',lunghezza: '7',},
+  {peso: '140',lunghezza: '9',},
+  {peso: '160',lunghezza: '10',},
+  {peso: '200',lunghezza: '12',},
+  {peso: '250',lunghezza: '14',},
+  {peso: '270',lunghezza: '13',},
+  {peso: '300',lunghezza: '15',},
+  {peso: '350',lunghezza: '16',},
+  {peso: '70',lunghezza: '5',}
 ];
+
+// Peso Totale delle zucchine
+
+let pesoTotale = 0;
+
+zucchine.forEach(zucchina => {
+  pesoTotale += parseInt(zucchina.peso);
+});
+
+console.log('Il peso totale delle zucchine é: ', pesoTotale);
+
+// JSnack 2
