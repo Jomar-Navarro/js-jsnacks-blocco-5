6997789a10f22dbdb215a0b24119a8cdb6c77fdd
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-03-07 16:59:12 +0100

    [add] array snack 1

--- js/script.js
+++ js/script.js
@@ -4,6 +4,52 @@ console.log('js-jsnacks-blocco-5');
 
 const zucchine = [
   {
-    
+    peso: '120',
+    lunghezza: '8',
+  },
+
+  {
+    peso: '100',
+    lunghezza: '7',
+  },
+
+  {
+    peso: '140',
+    lunghezza: '9',
+  },
+
+  {
+    peso: '160',
+    lunghezza: '10',
+  },
+
+  {
+    peso: '200',
+    lunghezza: '12',
+  },
+
+  {
+    peso: '250',
+    lunghezza: '14',
+  },
+
+  {
+    peso: '270',
+    lunghezza: '13',
+  },
+
+  {
+    peso: '300',
+    lunghezza: '15',
+  },
+
+  {
+    peso: '350',
+    lunghezza: '16',
+  },
+
+  {
+    peso: '70',
+    lunghezza: '5',
   }
-]
+];
