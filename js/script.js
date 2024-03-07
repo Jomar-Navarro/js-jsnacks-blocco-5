8925b2894fbb95c43c0dc51b1716e9e3d1ea9adc
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-03-07 17:23:46 +0100

    [add] Jsnack 2 ciclo forOf per dividere le lunghezza delle zucchine

--- js/script.js
+++ js/script.js
@@ -2,20 +2,19 @@ console.log('js-jsnacks-blocco-5');
 
 // JSnack 1
 const zucchine = [
-  {peso: '120',lunghezza: '8',},
-  {peso: '100',lunghezza: '7',},
-  {peso: '140',lunghezza: '9',},
-  {peso: '160',lunghezza: '10',},
-  {peso: '200',lunghezza: '12',},
-  {peso: '250',lunghezza: '14',},
-  {peso: '270',lunghezza: '13',},
-  {peso: '300',lunghezza: '15',},
-  {peso: '350',lunghezza: '16',},
-  {peso: '70',lunghezza: '5',}
+  {peso: '120', lunghezza: '8',},
+  {peso: '100', lunghezza: '7',},
+  {peso: '140', lunghezza: '9',},
+  {peso: '160', lunghezza: '10',},
+  {peso: '200', lunghezza: '12',},
+  {peso: '250', lunghezza: '17',},
+  {peso: '270', lunghezza: '15',},
+  {peso: '300', lunghezza: '16',},
+  {peso: '350', lunghezza: '18',},
+  {peso: '70',  lunghezza: '5',}
 ];
 
 // Peso Totale delle zucchine
-
 let pesoTotale = 0;
 
 zucchine.forEach(zucchina => {
@@ -25,3 +24,16 @@ zucchine.forEach(zucchina => {
 console.log('Il peso totale delle zucchine é: ', pesoTotale);
 
 // JSnack 2
+let zucchineMaggiori15 = [];
+let zucchineMinori15 = [];
+
+for (let zucchin of zucchine) {
+  if (zucchin.lunghezza <= 15) {
+    zucchineMinori15.push(zucchin);
+    console.log(zucchin);
+  }else{
+    zucchineMaggiori15.push(zucchin);
+    console.log(zucchin);
+  }
+};
+
